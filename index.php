9467e0ede98f7179270e713caa094e5929ad59a6
blackofjack <jeraldtamayo.ph@gmail.com>
2022-09-14 16:44:21 +0800

    test

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <?php wwp_head();?>
+</head>
+<body <?php body_class();?>>
+<?php wp_body_open();?>
+    <p>hello world!</p>
+
+    <?php wp_footer();?>
+</body>
+</html>
